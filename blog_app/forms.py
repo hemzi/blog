c0b30451f1fc13ebb85977b2a9e1b9bc62079614
hemzi <aaron.stacey@pm.me>
2022-05-25 15:47:24 -0500

    new post works

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Post
 
 
-class post_edit(forms.ModelForm):
+class PostEditForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'body']
-        labels = {'text': '', 'body': ''}
+        labels = {'title': '', 'body': ''}
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostEditForm
 # for testing
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+  '''return all posts in descending order'''
   posts = Post.objects.order_by('-created')
   context = {'posts':posts}
   return render(request, 'blog_app/home.html', context)
+
+def post_edit(request):
+  '''create/edit post'''
+  if request.method != 'POST':
+    # send empty form if GET
+    form = PostEditForm()
+  else:
+    form = PostEditForm(data=request.POST)
+    # check form and save to table
+    if form.is_valid():
+      form.save()
+      return redirect('blog_app:home')
+  context = {'form':form}
+  return render(request, 'blog_app/edit.html', context)
